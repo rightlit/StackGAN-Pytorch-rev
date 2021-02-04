b1a8716568f7e290d955ff9faf1f665d1b9d04bc
jeongwook lee <rightlit@gmail.com>
2021-02-04 15:07:18 +0900

    Update main.py

--- code/main.py
+++ code/main.py
@@ -72,6 +72,7 @@ if __name__ == "__main__":
         algo = GANTrainer(output_dir)
         algo.train(dataloader, cfg.STAGE)
     else:
-        datapath= '%s/test/val_captions.t7' % (cfg.DATA_DIR)
+        #datapath= '%s/test/val_captions.t7' % (cfg.DATA_DIR)
+        datapath= '%s/example_captions.t7' % (cfg.DATA_DIR)
         algo = GANTrainer(output_dir)
         algo.sample(datapath, cfg.STAGE)
